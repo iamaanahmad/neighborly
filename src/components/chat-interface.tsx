4410b4a59d4eedfc1e0777e61c2a9389579c583c
Amaan Ahmad <asharamaan234@gmail.com>
2025-09-13 10:26:08 +0000

    Failed to compile. ./workspace/src/components/chat-interface.tsx Error:

--- src/components/chat-interface.tsx
+++ src/components/chat-interface.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useEffect, useState, useRef, useTransition } from 'react';
@@ -389,6 +390,8 @@ export function ChatInterface() {
           </div>
         )}
       </div>
-    </div>
+    </Card>
   );
 }
+
+    
